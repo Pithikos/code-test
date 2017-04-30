71f3ef778e2d29b39c6cf546e56e82517be58407
Pithikos <manossef@hotmail.com>
2017-04-30 14:40:45 +0100

    Solve task

--- README.md
+++ README.md
@@ -1,66 +1,23 @@
-Ravelin Code Test
-=================
+Instructions
+------------
 
-## Summary
-We need an HTTP server that will accept any POST request (JSON) from muliple clients' websites. Each request forms part of a struct (for that particular visitor) that will be printed to the terminal when the struct is fully complete. 
+Run web server
 
-For the JS part of the test please feel free to use any libraries that may help you **but please only use the Go standard library for the backend**.
+    cd client
+    python -m SimpleHTTPServer 8000
 
-## Frontend (JS)
-Insert JavaScript into the index.html (supplied) that captures and posts data every time one of the below events happens; this means you will be posting multiple times per visitor. Assume only one resize occurs.
+Run application server
 
-  - if the screen resizes, the before and after dimensions
-  - copy & paste (for each field)
-  - time taken from the 1st character typed to clicking the submit button
+    export ALLOWED_ORIGIN=http://127.0.0.1:8000
+    go run server.go
 
-### Example JSON Requests
-```
-{
-  "eventType": "copyAndPaste",
-  "websiteUrl": "https://ravelin.com",
-  "sessionId": "123123-123123-123123123",
-  "pasted": true,
-  "formId": "inputCardNumber"
-}
-
-{
-  "eventType": "timeTaken",
-  "websiteUrl": "https://ravelin.com",
-  "sessionId": "123123-123123-123123123",
-  "time": 72, // seconds
-}
-
-...
-
-```
-
-## Backend (Go)
-
-The Backend must to:
-
-1. Create a Server
-2. Accept POST requests in JSON format similar to those specified above
-3. Map the JSON requests to relevant sections of the data struct (specified below)
-4. Print the struct for each stage of its construction
-5. Also print the struct when it is complete (i.e. when the form submit button has been clicked)
-
-### Go Struct
-```
-type Data struct {
-	WebsiteUrl         string
-	SessionId          string
-	ResizeFrom         Dimension
-	ResizeTo           Dimension
-	CopyAndPaste       map[string]bool // map[fieldId]true
-	FormCompletionTime int // Seconds
-}
-
-type Dimension struct {
-	Width  string
-	Height string
-}
-```
+Visit [index.html](http://127.0.0.1:8000) in your browser.
 
 
+Post-mortem
+-----------
 
+This is my first Go program so probably some things can be done much better. Also
+I tried to follow Go conventions but it's too much for the limited time.
 
+For debugging and testing I used Wireshark, CURL, python scripts, Firebug.
---
+++ server.go
@@ -0,0 +1,129 @@
+/*
+This is my first Go program so probably a few things are not made in the
+best way.
+*/
+
+package main
+
+import (
+    "os"
+    "fmt"
+    "log"
+    "io/ioutil"
+    "math/rand"
+    "strconv"
+    "net/http"
+    "encoding/json"
+)
+
+var ALLOWED_ORIGIN = os.Getenv("ALLOWED_ORIGIN")
+var CLIENT_DATA = make(map[string]*Data)
+
+
+type Dimension struct {
+    Width  string
+    Height string
+}
+
+type Data struct {
+    WebsiteUrl         string
+    SessionId          string
+    ResizeFrom         Dimension
+    ResizeTo           Dimension
+    CopyAndPaste       map[string]bool // map[fieldId]true
+    FormCompletionTime int `json:"time"`// Seconds
+}
+
+type JSONEvent struct {
+    EventType          string
+}
+
+type JSONPasteEvent struct {
+    *JSONEvent
+    Pasted             bool
+    FormId             string
+}
+
+
+// Handle payload from a client
+func payloadHandler(w http.ResponseWriter, r *http.Request, sessionId string) {
+
+    if _, ok := CLIENT_DATA[sessionId]; ! ok {
+        CLIENT_DATA[sessionId] = &Data{ CopyAndPaste: make(map[string]bool)}
+    }
+
+    // Probably inefficient. We use a buffer in order to be able and
+    // unmarshal the payload to two different structs. The memory expense
+    // for simple payloads is neglitible but there might a better way..
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        log.Printf("Could not read body content: %s", err)
+        http.Error(w, "Error reading request body", http.StatusInternalServerError)
+	}
+
+    err = json.Unmarshal(body, CLIENT_DATA[sessionId])
+    if err != nil {
+        log.Printf("Could not unmarshal Data: %s", err)
+        http.Error(w, "Internal error", http.StatusInternalServerError)
+    }
+
+    // Unmarshaling paste event needs a few more steps
+    var event JSONPasteEvent
+    err = json.Unmarshal(body, &event)
+    if err != nil {
+        log.Printf("Could not unmarshal PasteEvent: %s", err)
+        http.Error(w, "Internal error", http.StatusInternalServerError)
+	}
+    if event.EventType == "copyAndPaste" {
+        CLIENT_DATA[sessionId].CopyAndPaste[event.FormId] = event.Pasted
+    }
+
+    log.Printf("%+v\n", *CLIENT_DATA[sessionId])
+}
+
+
+// Handle client request
+func clientHandler(w http.ResponseWriter, r *http.Request) {
+
+    if r.Method == "POST" {
+
+        // Allow ONLY proper json
+        contentType := r.Header.Get("Content-type")
+        if contentType != "application/json" {
+            log.Printf("Not accepting content type '%s'\n", contentType)
+            http.Error(w, "Request content not supported", http.StatusNotAcceptable)
+        }
+
+        // Set session ID for client if needed
+        var sessionId string
+        cookie, _ := r.Cookie("sessionId")
+        if cookie == nil {
+            sessionId = strconv.Itoa(rand.Intn(100))
+            newCookie := http.Cookie{Name: "sessionId", Value:sessionId}
+            http.SetCookie(w, &newCookie)
+            fmt.Printf("Set new sessionId: %s\n", sessionId)
+        } else {
+            sessionId = cookie.Value
+        }
+
+        w.Header().Set("Access-Control-Allow-Origin", ALLOWED_ORIGIN)
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+        payloadHandler(w, r, sessionId)
+    } else if r.Method == "OPTIONS" { // Deal with CORS preflight
+        w.Header().Set("Access-Control-Allow-Origin", ALLOWED_ORIGIN)
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
+        w.Header().Set("Access-Control-Allow-Methods", "POST")
+        w.Header().Set("Access-Control-Max-Age", "1000")
+        w.Header().Set("Access-Control-Allow-Headers", "origin, x-csrf-token, content-type, accept")
+    } else {
+        log.Printf("Omitting method '%s'\n", r.Method)
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+    }
+}
+
+
+func main() {
+    http.HandleFunc("/", clientHandler)
+    http.ListenAndServe(":8080", nil)
+}
